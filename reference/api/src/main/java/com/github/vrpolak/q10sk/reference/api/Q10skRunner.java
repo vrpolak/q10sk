e2f5892b92b997bb0cfc12669926bf6e90c32efd
Vratko Polak <vratko.polak@gmail.com>
2017-06-24 07:53:10 +0200

    Introduce System interface
    Q10skRunner now accepts system argument,
    instead of separate bit consumer and supplier.

--- reference/api/src/main/java/com/github/vrpolak/q10sk/reference/api/Q10skRunner.java
+++ reference/api/src/main/java/com/github/vrpolak/q10sk/reference/api/Q10skRunner.java
@@ -31,7 +31,7 @@ package com.github.vrpolak.q10sk.reference.api;
 public interface Q10skRunner<NODE extends Q10skStateTreeRootNode> {
 
     /*
-     * Run a program, calling consumer and supplier for output and input repsectively, starting from initial state.
+     * Run a program, calling the system for output and input, starting from initial state.
      *
      * <p>If the program reaches a halted state, return the final state, otherwise keep executing indefinitely.
      *
@@ -39,11 +39,10 @@ public interface Q10skRunner<NODE extends Q10skStateTreeRootNode> {
      * which would enable program execution. It is assumed NODE type exposes such methods,
      * and runners using them would be of an interface extending this and documenting the execution method.
      *
-     * @param consumer  the consumer to be called when the program want to output, mutated on call
-     * @param supplier  the supplier to call when the program requires input, mutated on call
+     * @param system  the system to be called when the program wants to output or input, mutated on call
      * @param initialState  the initial state of the program, never changed
      * @return  the halted state
      */
-    NODE run(final BitConsumer consumer, final BitSupplier supplier, final NODE initialState);
+    NODE run(final System system, final NODE initialState);
 
 }
